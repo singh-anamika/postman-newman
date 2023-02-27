e28a5e26ff50d3874eceb49ee025049feaf72b68
anshuman0053 <93757971+anshuman0053@users.noreply.github.com>
2023-02-27 14:10:44 +0530

    Update xray.groovy

--- xray.groovy
+++ xray.groovy
@@ -13,13 +13,13 @@ pipeline {
         
         stage('Checkout'){
             steps{
-                checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: 'cd4433bd-73ee-4f3c-bb3c-c3f8e10144a8', url: 'https://github.com/singh-anamika/postman-xray.git']])
+                checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: '04bab23d-6247-42cd-a135-155a3e34f5b9', url: 'https://github.com/anshuman0053/postman-newman.git']])
             }
         }
         
         stage('Build Xray token python script'){
             steps{
-                git branch: 'main', credentialsId: 'cd4433bd-73ee-4f3c-bb3c-c3f8e10144a8', url: 'https://github.com/singh-anamika/postman-xray.git'
+                git branch: 'main', credentialsId: '04bab23d-6247-42cd-a135-155a3e34f5b9', url: 'https://github.com/anshuman0053/postman-newman.git'
                 //sh 'python3 postman_upload.py'
                 
                 script{
@@ -33,15 +33,18 @@ pipeline {
         
         stage('Running Postman API Collection via Newman'){
             steps{
-                sh "pwd"
-                git branch: 'main', credentialsId: 'cd4433bd-73ee-4f3c-bb3c-c3f8e10144a8', url: 'https://github.com/singh-anamika/postman-xray.git'
-                sh "newman run POC_google.json -r 'cli,junitfull,junitxray' --reporter-junitfull-export postman_xray_junitfull.xml --reporter-junitxray-export postman_xray_junitxray.xml -n 1"
+                git branch: 'main', credentialsId: '04bab23d-6247-42cd-a135-155a3e34f5b9', url: 'https://github.com/anshuman0053/postman-newman.git'
+                script{
+                        def newman_aut = sh(script: "parallel-execution-newman.py", returnStdout: true).trim()
+                        
+                }
+                  
             }
         }
         stage('Uploading Newman reports to Xray JIRA'){
             steps{
                 sh "pwd"
-                sh 'curl -H "Content-Type: text/xml" -X POST -H "Authorization: $BEARER_TOKEN" --data @postman_xray_junitfull.xml https://xray.cloud.getxray.app/api/v2/import/execution/junit?projectKey=CQ'
+                sh 'curl -H "Content-Type: text/xml" -X POST -H "Authorization: $BEARER_TOKEN" --data @newman_junitfull.xml https://xray.cloud.getxray.app/api/v2/import/execution/junit?projectKey=CQ'
             }
         }
         
