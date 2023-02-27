2b03b3290c91f4248d2bbfab8ead180cc624e0b7
anshuman0053 <93757971+anshuman0053@users.noreply.github.com>
2023-02-27 14:30:22 +0530

    Update parallel-execution-newman.py

--- parallel-execution-newman.py
+++ parallel-execution-newman.py
@@ -10,7 +10,7 @@ random.shuffle(collection['item'])
 chunks = [collection['item'][i:i+10] for i in range(0, len(collection['item']), 5)]
 
 for i, chunk in enumerate(chunks):
-    command = f"newman run CVC_Apis.postman_collection.json --env-var concurrency={len(chunk)} --reporters cli,junit --reporter-junit-export junitfull.xml"
+    command = f"newman run CVC_Apis.postman_collection.json --env-var concurrency={len(chunk)} --reporters cli,junit --reporter-junit-export report.xml"
     proc = subprocess.Popen(command, shell=True)
     proc.wait()
 
