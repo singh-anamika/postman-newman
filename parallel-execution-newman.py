060196b16380260080efa129d8d75885f64783bb
anshuman0053 <93757971+anshuman0053@users.noreply.github.com>
2023-03-13 12:56:02 +0530

    Update parallel-execution-newman.py

--- parallel-execution-newman.py
+++ parallel-execution-newman.py
@@ -1,6 +1,9 @@
 import subprocess
 import json
 import random
+import sys
+
+collection_url = sys.argv[1]
 
 with open('CVC_Apis.postman_collection.json', 'r') as f:
     collection = json.load(f)
@@ -10,7 +13,7 @@ random.shuffle(collection['item'])
 chunks = [collection['item'][i:i+10] for i in range(0, len(collection['item']), 20)]
 
 for i, chunk in enumerate(chunks):
-    command = f"newman run https://api.postman.com/collections/18481634-926bcc97-e8db-4354-ac57-e293cd106587?access_key=PMAT-01GV2Y8JN6HSKEW0PRMRKK8GVA --env-var concurrency={len(chunk)} --reporters cli,junit --reporter-junit-export report.xml"
+    command = f"newman run {collection_url} --env-var concurrency={len(chunk)} --reporters cli,junit --reporter-junit-export report.xml"
     proc = subprocess.Popen(command, shell=True)
     proc.wait()
 
